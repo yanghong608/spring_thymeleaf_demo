aa1ed5a163889c1a01e99d9f7e1a27f10a343ad9
yanghong <yanghong@offcn.com>
2020-10-28 08:42:29 +0800

    创建一个测试文件

---
+++ src/main/java/com/offcn/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.offcn.controller;
+
+/**
+ * @Auther: yh
+ * @Date: 2020/10/27
+ * @Description:
+ */
+public class TestController {
+
+    public String test(){
+        return null;
+    }
+}
